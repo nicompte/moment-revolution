502276d3debf75985ed0bf24ce231322e49aa36f
Nicolas Barbotte <nicolas@barbotte.net>
2014-08-23 15:10:10 +0200

    calendar

--- dev/assets/scripts/main.js
+++ dev/assets/scripts/main.js
@@ -10,14 +10,25 @@ Date.prototype.toDateInputValue = function() {
 
 $(function (){
 
-  $('#date').on('change', function(){
-    var b = $(this).val().split(/\D/);
-    var date = new Date(b[0], --b[1], b[2]);
+
+  $('#date').datetimepicker({
+    format: 'dd/mm/yyyy',
+    minView: 2,
+    language: 'fr',
+    autoclose: true,
+    todayBtn: true
+  })
+  .on('change', function(){
+    var date = moment($(this).val(), 'DD/MM/YYYY');
     var revDate = moment(date).toRevolutionnary();
     $('#revdate').html(revDate);
+  })
+  .val(moment(new Date()).format('DD/MM/YYYY'))
+  .siblings('.input-group-addon').on('click', function(){
+    $('#date').datetimepicker('show');
   });
 
-  $('#date').val(new Date().toDateInputValue());
   var revDate = moment(new Date()).toRevolutionnary();
   $('#revdate').html(revDate);
+
 });
